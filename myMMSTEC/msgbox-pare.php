94ed19bde7871ccb062e2faeeb8b186bcc79afd3
Marcos Morais <mmstec@gmail.com>
2022-08-22 10:05:40 -0300

    update
    Atualização da pasta myMMSTEC

---
+++ myMMSTEC/msgbox-pare.php
@@ -0,0 +1,198 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>mmstec</title>
+	<embed src="./sirene.mp3" width="0" height="0" autostart="true" hidden="true" loop=0></embed>
+	
+
+
+	<style type="text/css">
+	<!--
+	body{
+		text-align:center;
+		background:#FFFFFF;
+		font-family:verdana,arial,helvetica,sans-serif;
+		font-size:80%;
+		padding:0px;
+		margin:0px;
+	}
+	container{
+		background:#E2001A;
+	}
+
+	#top{
+		width:770px;
+		text-align:left;
+		margin:10px auto 10px auto;
+		background-color: #F5F5F5;
+		background-repeat:no-repeat;
+		background-position:630px 50px;
+		border-left:0px solid #ffffff;
+		border-right:0px solid #ffffff;
+		}
+	#rodape{
+		width:770px;
+		text-align:left;
+		margin:10px auto 10px auto;
+		background-color: #F5F5F5;
+		background-repeat:no-repeat;
+		background-position:630px 50px;
+		border-left:0px solid #ffffff;
+		border-right:0px solid #ffffff;
+		}
+	#conteudo{
+		width:770px;
+		text-align:left;
+		margin:10px auto 10px auto;
+		background-color: #F5F5F5;
+		background-repeat:no-repeat;
+		background-position:630px 50px;
+		border-left:0px solid #ffffff;
+		border-right:0px solid #ffffff;
+		}
+	h1 {
+		position: absolute;
+		top: 40px;
+		left: 25px;
+		color: #000000;
+		margin: 0px;
+		padding: 0px;
+		font: normal 40px/100% "Trebuchet MS", Arial, sans-serif;
+	}
+
+	h1 a, h1 a:visited {
+		color: #F1F1F1;
+		text-decoration: none;
+
+	}
+	h1 a:hover {
+		color: #000000;
+		text-decoration: none;
+	}
+	h2 {
+		color: #333333;
+		margin: 3px 0 2px;
+		padding-bottom: 4px;
+		font: normal 150%/100% "Lucida Grande", Arial, sans-serif;
+		border-bottom: dotted 1px #CCCCCC;
+	}
+	h2 a, h2 a:visited {
+		color: #333333;
+		text-decoration: none;
+	}
+	h2 a:hover {
+		color: #44a1d0;
+		text-decoration: none;
+	}
+	h3 {
+		color: #666666;
+		margin: 5px 0px 5px;
+		font: bold 110%/110% "Lucida Grande", Arial, sans-serif;
+	}
+
+	.description {
+		position: absolute;
+		top: 77px;
+		left: 25px;
+		color: #000000;
+		font: 12px Arial, Helvetica, sans-serif;
+	}
+
+	#centralizar
+	{
+		background-color:Transparent;
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		margin-top: -270px; 
+		margin-left: -480px; 
+		width: 960px;
+		height: 525px;
+		z-index: 1;
+	}
+	#caixatexto {
+		position:absolute;
+		top: 3px;
+		left:403px;
+		font-family:Segoe UI, Tahoma;
+		font-size:40px;
+		letter-spacing:-1px;
+		text-align:justify;
+		width:560px;
+		height:380px;
+	}
+	#texto1 {	
+		position: absolute;
+		top: 80px;
+		left: 25px;
+		margin-top: 20px;
+		margin-right: 10px;
+		padding: 0px;
+		font-family:Segoe UI Light, Tahoma;
+		font-size:30px;
+		letter-spacing:-1px;
+		text-align:justify;
+		color:#000000;
+	}
+	#texto2 {	
+		position: absolute;
+		top: 80px;
+		left: 25px;
+		margin-top: 314px;
+		margin-right: 200px;
+		margin-left: -400px;
+		padding: 0px;
+		font-family:Segoe UI Light, Tahoma;
+		font-size:30px;
+		letter-spacing:-1px;
+		text-align:justify;
+		color:#F1F1F1;
+	}
+		
+	.texto1 a { color:#F1F1FF; }
+	#texto3
+	{
+		border:1px;
+		border-color: #FFF2D7 #FF9900 #FF9900 #FFF2D7;
+		font-size: 12px;
+		font-family: Verdana;
+		font-weight: normal;
+		background-color: #FFCC66;
+		position: relative;
+		padding: 5px;
+		-moz-border-radius: 4px;
+		font-weight: normal;
+		color: Black;
+		text-align:center;
+		margin-top: 7px;
+	}
+	-->
+	</style>
+	</head>
+	<body>
+		<div id="conteudo" class="centralizar" align="left">
+			<div id="centralizar">
+				<table width="100%" >
+					<tr align="center">
+						<td valign="baseline">
+							<img src="msgbox-pare.png" alt="" width="960" height="523" border="0" usemap="#voltar" />
+							<div id="caixatexto">
+								<h1>ACESSO NÃO PERMITIDO</h1>
+								<div id="texto1"></div>
+								<div id="texto2">Desculpe, mas a página solicitada não esta relacionada na lista de interesses da empresa. <br /><br />Por favor, tente outra página.</div>
+							</div>
+							<div id="texto3">Esta solução foi elaborada e implementada por Marcos M Sousa.</div>
+							<map name="voltar" id="voltar">
+								<area shape="rect" coords="193,45,567,163" href="../" target="_self" alt="clique para voltar" />
+							</map>
+						</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+		<div id="rodape" align="left"></div>
+	</body>
+</html>
